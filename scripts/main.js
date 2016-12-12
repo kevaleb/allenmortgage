62c4841d12dcf4cb398aa1b96a7690ee6755635a
Kevin LeBaron <kevaleb@gmail.com>
2016-12-12 16:57:36 -0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,96 @@
+$(document).ready(function(){	
+	$("header > .menu-button").click(function(){
+	    $("header").toggleClass("open");
+	});
+});
+
+
+$(function() {
+	
+	$(document).ready(function(){
+	
+		// Open overlay
+		$("#close-overlay").click(function(){
+		    $(".overlay").toggleClass("visible");
+		});
+		
+		// Close overlay
+		$("#open-overlay").click(function(){
+		    $(".overlay").toggleClass("visible");
+		});
+//		
+//		$("body > header > div > .menu-button").click(function(){
+//		    $("body > header > div > nav").toggleClass("visible");
+//		});
+		
+//		mixpanel.track('Viewed', {
+//	//	    'page name' : document.title,
+//		    'url' : window.location.pathname
+//		});
+		
+		
+	});
+	
+	
+	
+	// Scroll To #
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+	
+	
+	
+	// Find all YouTube videos
+	var $allVideos = $("iframe[src^='https://www.youtube.com']"),
+
+	    // The element that is fluid width
+	    $fluidEl = $(".video-wrapper");
+
+	// Figure out and save aspect ratio for each video
+	$allVideos.each(function() {
+
+		$(this)
+			.data('aspectRatio', this.height / this.width)
+			
+			// and remove the hard coded width/height
+			.removeAttr('height')
+			.removeAttr('width');
+
+	});
+
+	// When the window is resized
+	// (You'll probably want to debounce this)
+	$(window).resize(function() {
+
+		var newWidth = $fluidEl.width();
+		
+		// Resize all videos according to their own aspect ratio
+		$allVideos.each(function() {
+
+			var $el = $(this);
+			$el
+				.width(newWidth)
+				.height(newWidth * $el.data('aspectRatio'));
+
+		});
+
+	// Kick off one resize to fix all videos on page load
+	}).resize();
+	
+	
+	
+	
+	
+	
+});
